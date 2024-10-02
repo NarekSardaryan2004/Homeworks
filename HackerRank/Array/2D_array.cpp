84f65929a4102197c83f356df2d2026276d71083
Narek Sardaryan <165571152+NarekSardaryan2004@users.noreply.github.com>
2024-10-02 20:35:31 +0400

    Update 2D_array.cpp

--- HackerRank/Array/2D_array.cpp
+++ HackerRank/Array/2D_array.cpp
@@ -1,1 +1,14 @@
-
+int hourglassSum(vector<vector<int>> arr) {
+    int max_sum = INT_MIN;
+    for (int i = 0; i <= 3; ++i) {
+        for (int j = 0; j <= 3; ++j) {
+            int sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] +
+            arr[i + 1][j + 1] +
+            arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+            
+            max_sum = max(max_sum, sum);
+        }
+    }
+    
+    return max_sum;
+}
